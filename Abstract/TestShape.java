674affbd20de2824b75d8339725c93e8ebab7e04
Deepak Patidar <deepaksariya20070@gmail.com>
2023-07-17 12:14:51 +0530

    done;

---
+++ Abstract/TestShape.java
@@ -0,0 +1,24 @@
+package core.java.Abstract;
+
+public class TestShape {
+
+	public static void main(String[] args) {
+		 
+		 Recangle r = new Recangle();
+		 
+		 r.area();
+		 
+		/*
+		 *  Ashape s = new Recangle();
+		 
+		 s.area();
+		 
+		 */
+		 
+		 Ashape.sum();
+		 
+		 
+		 
+		
+	}
+}
